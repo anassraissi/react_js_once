49757eb1b8a7e280aa558b65fc1c13e7c2f458be
anassraissi <https://github.com/anassraissi/vue_js-.git>
2022-07-06 20:19:49 +0100

    local component parametre : state

--- src/wellcome_as_class_component.js
+++ src/wellcome_as_class_component.js
@@ -1,9 +1,19 @@
 import React,{Component} from "react";
 
 class Wellcome_as_class_component extends Component{
+    state ={   //state as local parametre on using in component
+        age:14
+    }  
+    user={
+        name:'Anass',
+        lastname:'Raissi',
+    }
+        socialsLinks=['facebook','twitter'];
     render(){  //componant as class external file
-        return <div>hi {this.props.name+' '+this.props.lastname} im from wellcome_as_class_component 
-        <p>{this.props.children} add the value of declaring component</p> 
+        return <div>hi {this.props.name+' '+this.props.lastname} im from wellcome_as_class_component my age is  
+        {this.state.age}
+        <p>{this.props.children} add the value of declaring component</p> <br></br>
+         <p>your name : {this.user.name} and  lastname : {this.user.lastname} and about socials site is {this.socialsLinks}</p>
         </div>
     }
 } 
