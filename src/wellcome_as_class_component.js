4d877acd8259a966248265a39d151123f5bb87e2
anassraissi <https://github.com/anassraissi/vue_js-.git>
2022-07-06 21:47:27 +0100

    basics event update state

--- src/wellcome_as_class_component.js
+++ src/wellcome_as_class_component.js
@@ -1,6 +1,11 @@
 import React,{Component} from "react";
 
 class Wellcome_as_class_component extends Component{
+
+        constructor(props){
+                super(props)
+                this.decrement_by_function=this.decrement_by_function.bind(this)  //for not reputing bind(this) in each use of it // this mean send class to it
+        }
     state ={   //state as local parametre on using in component
         age:14
     }  
@@ -8,13 +13,23 @@ class Wellcome_as_class_component extends Component{
         name:'Anass',
         lastname:'Raissi',
     }
+    decrement=()=>{
+       {this.setState({age:this.state.age-1})}
+    }
+    decrement_by_function(){  //this parametre serve to use class atrebute in function like that
+       {this.setState({age:this.state.age-1})}  
+    }
         socialsLinks=['facebook','twitter'];
     render(){  //componant as class external file
         return <div>hi {this.props.name+' '+this.props.lastname} im from wellcome_as_class_component my age is  
         {this.state.age}
         <p>{this.props.children} add the value of declaring component</p> <br></br>
          <p>your name : {this.user.name} and  lastname : {this.user.lastname} and about socials site is {this.socialsLinks}</p>
-        </div>
+         <button onClick={()=>{this.setState({age :this.state.age+1})}}>Incresse Age</button>
+         <button onClick={this.decrement}>Decresse</button>
+         <button onClick={this.decrement_by_function.bind(this)}>Decresse by fucntion</button> 
+         <button onClick={this.decrement_by_function}>Decresse by fucntion constructor</button> 
+        </div> 
     }
 } 
 export default Wellcome_as_class_component;
