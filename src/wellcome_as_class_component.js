779a8b192b8ace6c18843a5f2ac977b24ec3e7bb
anassraissi <https://github.com/anassraissi/vue_js-.git>
2022-07-06 22:22:19 +0100

    basics onchange event

--- src/wellcome_as_class_component.js
+++ src/wellcome_as_class_component.js
@@ -7,28 +7,43 @@ class Wellcome_as_class_component extends Component{
                 this.decrement_by_function=this.decrement_by_function.bind(this)  //for not reputing bind(this) in each use of it // this mean send class to it
         }
     state ={   //state as local parametre on using in component
-        age:14
+        age:14,
+        user:{
+            name:'Anass',
+            lastname:'Raissi',
+        }
     }  
-    user={
-        name:'Anass',
-        lastname:'Raissi',
-    }
+   
     decrement=()=>{
        {this.setState({age:this.state.age-1})}
     }
     decrement_by_function(){  //this parametre serve to use class atrebute in function like that
        {this.setState({age:this.state.age-1})}  
     }
+    update_name(event){
+        console.log(event.target.value);
+        const new_name=event.target.value;
+        this.setState({
+                user:{
+                    ...this.state.user,  //bach man3awdoch ga3 data li m3aha ila bghina nbadlo ghawahed  bhal lastname:this.state.user.lastname
+                    name:new_name,
+                    // lastname:this.state.user.lastname
+                }
+        });
+
+    }
         socialsLinks=['facebook','twitter'];
     render(){  //componant as class external file
         return <div>hi {this.props.name+' '+this.props.lastname} im from wellcome_as_class_component my age is  
         {this.state.age}
         <p>{this.props.children} add the value of declaring component</p> <br></br>
-         <p>your name : {this.user.name} and  lastname : {this.user.lastname} and about socials site is {this.socialsLinks}</p>
-         <button onClick={()=>{this.setState({age :this.state.age+1})}}>Incresse Age</button>
+         <p>your name : {this.state.user.name} and  lastname : {this.state.user.lastname} and about socials site is {this.socialsLinks}</p>
+         <button onClick={()=>{this.setState({age :this.state.age+1})}}>Incresse Age</button> 
          <button onClick={this.decrement}>Decresse</button>
          <button onClick={this.decrement_by_function.bind(this)}>Decresse by fucntion</button> 
          <button onClick={this.decrement_by_function}>Decresse by fucntion constructor</button> 
+         <br></br>
+         <input type='text' value={this.state.user.name} onChange={this.update_name.bind(this)}></input>
         </div> 
     }
 } 
