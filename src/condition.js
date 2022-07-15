18ae12b42673c8f7acda0f557dd97493df46c001
anassraissi <https://github.com/anassraissi/vue_js-.git>
2022-07-15 21:53:09 +0100

    print array of object by map

--- src/condition.js
+++ src/condition.js
@@ -9,16 +9,25 @@ function Conditions(props){
 
     }
     else{
-        // afficher='you are poor man';
+        // afficher='you are poor man';        afficher=<h1>You are rich man<h1/>;
+
         afficher=<h1>You are poor man bro</h1>;
 
     }
+    let users=[
+        {id:1,name:'Anass',age:27},
+        {id:2,name:'marouane',age:26},
+        {id:3,name:'ALi',age:25}
+    ]
         return <div>
             <hr></hr>
             {age>12 ? <div>you are older than 12</div> : <div>you are smaller than 12</div> } 
             {age>22 ? <div>you are older than 22</div> : null } 
             {afficher}
-
+            <ul>
+            {users.map((users,index) => <li  key={users.id}>{index} : {users.name}</li>)}
+            </ul>
+ 
         </div>
    
 }
